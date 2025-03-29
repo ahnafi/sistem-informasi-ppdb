94857624465d4bdfe4920ea4de8c13a10894e818
ahnafi <devsulthon@gmail.com>
2025-03-29 16:31:57 +0700

    feat :modify filament resource article

--- app/Filament/Resources/ArticleResource.php
+++ app/Filament/Resources/ArticleResource.php
@@ -6,44 +6,69 @@ use App\Filament\Resources\ArticleResource\Pages;
 use App\Filament\Resources\ArticleResource\RelationManagers;
 use App\Models\Article;
 use Filament\Forms;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class ArticleResource extends Resource
 {
     protected static ?string $model = Article::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationLabel = 'Artikel';
+    protected static ?string $navigationGroup = 'Manajemen Artikel';
+    protected static ?string $navigationIcon = 'heroicon-o-newspaper';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('title')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('slug')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\Textarea::make('content')
-                    ->required()
-                    ->columnSpanFull(),
-                Forms\Components\TextInput::make('thumbnail')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\Toggle::make('is_featured')
-                    ->required(),
-                Forms\Components\Select::make('category_id')
-                    ->relationship('category', 'title')
-                    ->required(),
-                Forms\Components\Select::make('author_id')
-                    ->relationship('author', 'name')
-                    ->required(),
-            ]);
+                Forms\Components\Split::make([
+                    Section::make([
+                        TextInput::make('title')
+                            ->required()
+                            ->maxLength(255)
+                            ->reactive()
+                            ->afterStateUpdated(function ($state, callable $set) {
+                                $slug = Str::slug($state);
+                                $count = 1;
+                                while (Article::where('slug', $slug)->exists()) {
+                                    $slug = Str::slug($state) . '-' . $count++;
+                                }
+                                $set('slug', $slug);
+                            }),
+
+                        TextInput::make('slug')
+                            ->readOnly()
+                            ->unique(ignoreRecord: true),
+
+                        Forms\Components\RichEditor::make('content')
+                            ->fileAttachmentsDirectory("articles")
+                            ->required()
+                            ->columnSpanFull(),
+                    ]),
+                    Section::make([
+                        Forms\Components\FileUpload::make('thumbnail')
+                            ->image()
+                            ->imageEditor()
+                            ->maxSize(2048)
+                            ->directory('articles')
+                            ->required(),
+                        Forms\Components\Toggle::make('is_featured'),
+                        Forms\Components\Select::make('category_id')
+                            ->relationship('category', 'title')
+                            ->required(),
+                        Forms\Components\Select::make('author_id')
+                            ->relationship('author', 'name')
+                            ->required(),
+                    ])->grow(false),
+                ])->from("md"),
+            ])->columns(1);
     }
 
     public static function table(Table $table): Table
@@ -56,14 +81,14 @@ class ArticleResource extends Resource
                     ->searchable(),
                 Tables\Columns\TextColumn::make('thumbnail')
                     ->searchable(),
-                Tables\Columns\IconColumn::make('is_featured')
-                    ->boolean(),
                 Tables\Columns\TextColumn::make('category.title')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('author.name')
                     ->numeric()
                     ->sortable(),
+                Tables\Columns\ToggleColumn::make('is_featured')
+                    ->boolean(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
