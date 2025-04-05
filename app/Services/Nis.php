9931957a3a9f73faeb76799e0374afb2681abef6
ahnafi <devsulthon@gmail.com>
2025-04-05 15:30:28 +0700

    fix : nis to long,  cut year Y to y

--- app/Services/Nis.php
+++ app/Services/Nis.php
@@ -8,8 +8,8 @@ class Nis
 {
     public static function generate(int $registrationId, Carbon $birth): string
     {
-        $year = now()->format('Y');
-        $birthFormatted = $birth->format('dmY');
+        $year = now()->format('y');
+        $birthFormatted = $birth->format('dmy');
         $regId = str_pad($registrationId, 3, '0', STR_PAD_LEFT);
 
         return $year . $birthFormatted . $regId;
