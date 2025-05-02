ac62393e6855da32d0f0863bb505cb524a12f92a
ahnafi <devsulthon@gmail.com>
2025-05-02 18:11:53 +0700

    feat : seeder article and adding academic calendar

---
+++ app/Filament/Resources/AcademicCalendarResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AcademicCalendarResource\Pages;
+use App\Filament\Resources\AcademicCalendarResource\RelationManagers;
+use App\Models\AcademicCalendar;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AcademicCalendarResource extends Resource
+{
+    protected static ?string $model = AcademicCalendar::class;
+    protected static ?string $navigationLabel = "Kalender Akademik";
+    protected static ?string $navigationGroup = "Manajemen Kalender Akademik";
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title'),
+                Forms\Components\KeyValue::make('agenda')
+                    ->keyLabel('Tanggal')
+                    ->valueLabel("kegiatan")
+                    ->columnSpanFull()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title'),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAcademicCalendars::route('/'),
+            'create' => Pages\CreateAcademicCalendar::route('/create'),
+            'edit' => Pages\EditAcademicCalendar::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        
+        $title = $this->faker->sentence(6);
+        return [
+            'title' => $title,
+            'slug' => Str::slug($title) . '-' . Str::random(5),
+            'content' => $this->faker->paragraphs(5, true),
+            'thumbnail' => $this->faker->imageUrl(800, 600, 'news', true, 'thumbnail'),
+            'is_featured' => $this->faker->boolean(20),
+            'category_id' => \App\Models\Category::inRandomOrder()->first()->id ?? \App\Models\Category::factory(),
+            'author_id' => \App\Models\Author::inRandomOrder()->first()->id ?? \App\Models\Author::factory(),
+        ];
+    }
+}
